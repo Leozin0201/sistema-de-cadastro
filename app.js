f86a154ba4c4d431ba3e039fb5fcb561b81e83e7
unknown <you@example.com>
2022-06-21 14:40:48 -0300

    Testando no JS

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("HORA DO HEROI, HORA DO GUSTAVO")
 console.log("Mais um teste para alteração de arquivos e comittar")
+console.log("Enviando mais um commit para o LEÓ")
